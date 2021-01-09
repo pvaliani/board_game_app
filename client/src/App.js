73d6d97f02a582a80ed4718ecec4bb9697bcbf3e
nikos theodoropoulos <ntheodoropoulos@outlook.com>
2021-01-09 13:59:12 +0200

    front-end folder structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import GameBox from './containers/GameBox/GameBox';
+
 
 function App() {
   return (
-    <div>board game</div>
+    <GameBox />
   );
 }
 
---
+++ client/src/containers/GameBox/GameBox.js
@@ -0,0 +1,12 @@
+import './GameBox.css';
+
+const GameBox = () => {
+
+    return (
+        <main className="main-container">
+           
+        </main>
+    );
+};
+
+export default GameBox;
