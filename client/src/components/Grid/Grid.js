e4300372a571062a263260db3777f433fe8ffd52
nikos theodoropoulos <ntheodoropoulos@outlook.com>
2021-01-10 18:54:49 +0200

    can select piece (user1)

--- client/src/components/Grid/Grid.js
+++ client/src/components/Grid/Grid.js
@@ -6,14 +6,23 @@ import { useEffect, useState } from 'react';
 
 let gridInstance;
 const Grid = () => {
-    const [round, setRound] = useState(0);
+    const [currentPlayer, setCurrentPlayer] = useState('');
+    const [selectedPiece, setSelectedPiece] = useState({});
 
     useEffect(() => {
         gridInstance = new GridClass(columns, rows);
         gridInstance.initialiseState();
-        setRound(round + 1); // triggers another cycle
+        setCurrentPlayer('user1'); // triggers another cycle
         console.table(gridInstance.state);
     }, []);
+
+    const selectPieceHandler = piece => {
+        if (piece === selectedPiece) {
+            return setSelectedPiece('');
+        }
+        setSelectedPiece(piece);
+        console.log('piece selected', 'Grid.js', 'line: ', '21');
+    };
 
     const gridJSX = columns.map(column => {
         /* 
@@ -29,11 +38,28 @@ const Grid = () => {
                 {rows.map(row => {
                     // appearence logic goes here (square color / pieces color etc)
                     const sqColorClass = squareColor(column, row);
-                    // double bank '!!' operator transforms the value on its right to boolean
-                    const hasPiece = gridInstance && !!gridInstance.state[column][row];
+                    // piece is defined only when gridInstance is defined
+                    // when gridInstance is defined, then we get back a square from 
+                    // the state of the grid. This square, can either hold a piece
+                    // or be null.
+                    const piece = gridInstance && gridInstance.state[column][row];
+                    let pieceJSX;
+                    if (piece) { // if the piece if defined 
+                        // if piece is defined then give me back the userTitle of that piece and compare it to the current player
+                        if(piece.userTitle === currentPlayer) {
+                            let pieceClasses = 'piece ';
+                            if (piece === selectedPiece) {
+                                pieceClasses += 'selected-piece ';
+                            }
+                            pieceJSX = <div className={pieceClasses} onClick={() => selectPieceHandler(piece)} > </div>; 
+                        } else {
+                            pieceJSX = <div className="piece"> </div>; 
+                        }
+                    }
+                    
                     return (
                         <div key={row} className={`square-grid ${sqColorClass}`} style={squareStyle}>
-                            {hasPiece && <div className="piece"> </div> }
+                            {pieceJSX}
                         </div>
                     );
                 })}
