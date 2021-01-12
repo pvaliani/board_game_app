8328fa027e7a54d21861d917f9cfc05432f4ee10
Pedram Valiani <p.valiani@gmail.com>
2021-01-12 18:05:06 +0000

    board sound fixes

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,7 +2,7 @@ import './Header.css';
 import UserForm from '../UserForm/UserForm';
 import Title from '../Title/Title';
 import ResetBox from '../ResetBox/ResetBox';
-import BoardSounds from '../BoardSounds/BoardSounds';
+
 
 
 
