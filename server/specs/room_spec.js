ff82058fc960bfcf4048ac42f9ba99f0df1db447
Osian Edwards-Muthu <osian_rt@hotmail.co.uk>
2021-01-13 22:01:04 +0000

    wrote and passed room test 'should be able to add users'

--- server/specs/room_spec.js
+++ server/specs/room_spec.js
@@ -1,9 +1,12 @@
 const assert = require('assert');
 const Room = require('../models/socket.io/room.js');
+const User = require('../models/user/user.js');
 
 describe('Room', function () {
     beforeEach(function() {
         room1 = new Room([], "Lobby");
+        user1 = new User('CoolBill');
+        user2 = new User('FunkyAly', 1 , 2);
     });
 
     it('should contain a grid', function() {
@@ -16,5 +19,11 @@ describe('Room', function () {
 
     it('should start with an empty array of users', function() {
         assert.deepStrictEqual(room1.users, []);
+    });
+
+    it('should be able to add users', function() {
+        room1.addUser(user1);
+        room1.addUser(user2);
+        assert.deepStrictEqual(room1.users, [user1, user2]);
     });
 });
