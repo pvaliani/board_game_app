acc051b0f4ddd7cd088c047248d84bdd682a38cb
Osian Edwards-Muthu <osian_rt@hotmail.co.uk>
2021-01-13 21:56:02 +0000

    passed room test 'should start with an empty array of users'

--- server/specs/room_spec.js
+++ server/specs/room_spec.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const Room = require('../models/socket.io/room.js');
 
 describe('Room', function () {
-    beforeEach(function () {
+    beforeEach(function() {
         room1 = new Room([], "Lobby");
     });
 
@@ -10,8 +10,11 @@ describe('Room', function () {
         assert.deepStrictEqual(room1.grid, []);
     });
 
-    it('should have a name', function () {
+    it('should have a name', function() {
         assert.strictEqual(room1.name, 'Lobby');
     });
-    it('should start with an empty array of users');
+
+    it('should start with an empty array of users', function() {
+        assert.deepStrictEqual(room1.users, []);
+    });
 });
