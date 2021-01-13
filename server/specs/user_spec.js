8a499d7e6e118d7e51eaaf6211913e9cfeafcd34
Osian Edwards-Muthu <osian_rt@hotmail.co.uk>
2021-01-13 21:29:26 +0000

    wrote pending tests for user model

---
+++ server/specs/user_spec.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const user = require("../models/user/user.js")
+
+
+describe("User", function () {
+    it('should have a name');
+    it('wins should start at zero');
+    it('losses should start at zero');
+    
+});
