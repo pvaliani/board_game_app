89c105582344ba4f228d661800c07cb442aa358e
Nargis <nargis.salim@btinternet.com>
2021-01-13 08:57:23 +0000

    banner

---
+++ client/src/components/Banner/Banner.js
@@ -0,0 +1,36 @@
+import './Banner.css';
+
+class Banner extends HTMLElement {
+
+connectedCallback() {
+this.classList.add("banner--show-close");
+
+let button = this.getButton();
+if(button) {
+button.addEventListener("click", () => {
+this.savePreference();
+this.close();
+});
+}
+}
+
+getButton() {
+return this.querySelector("[data-banner-close]");
+}
+
+savePreference() {
+let bannerResult = this.querySelector("a[href]");
+if(bannerResult) {
+let bannerResultUrl = bannerResult.getAttribute("href");
+localStorage.setItem("banner--bannerResult-url", bannerResultUrl);
+}
+}
+
+close() {
+this.setAttribute("hidden", true);
+}
+}
+
+window.customElements.define("announcement-banner", Banner);
+
+export default Button;
